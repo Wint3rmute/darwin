a8bc6d1b8dbfec28d6ef153340eaade8a7810626
wint3rmute <mateusz.baczek1998@gmail.com>
2025-10-18 16:25:31 +0200

    minor helix config

--- modules/helix.nix
+++ modules/helix.nix
@@ -13,6 +13,20 @@
       theme = "base16_transparent"
       [editor]
       line-number = "relative"
+      
+      [keys.normal]
+      "0" = "goto_line_start"
+      "C-r" = "redo"
+      # "g" = ":reflow"
+      # ":e" = ":reload"
+
+      "}" = "goto_next_paragraph"
+      "{" = "goto_prev_paragraph"
+
+      [keys.normal.space]
+      "q" = ":quit"
+      "/" = "toggle_comments"
+      "s" = "global_search"
     '';
   };
 }
