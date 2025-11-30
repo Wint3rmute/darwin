26b76115d2ae935163c884beac264f433ce489ea
wint3rmute <mateusz.baczek1998@gmail.com>
2025-11-30 09:45:07 +0100

    block spotify updates

--- modules/global_packages.nix
+++ modules/global_packages.nix
@@ -33,4 +33,22 @@
     tmux
     vscode
   ];
+
+  environment.etc."hosts" = {
+    text = ''
+      ##
+      # Host Database
+      #
+      # localhost is used to configure the loopback interface
+      # when the system is booting.  Do not change this entry.
+      ##
+      127.0.0.1       localhost
+      255.255.255.255 broadcasthost
+      ::1             localhost
+
+      # Block Spotify updates
+      127.0.0.1 upgrade.spotify.com
+      127.0.0.1 upgrade.scdn.co
+    '';
+  };
 }
