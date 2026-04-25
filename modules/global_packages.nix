01175d900aff28e49a960f6014e7168bb51da35f
wint3rmute <mateusz.baczek1998@gmail.com>
2026-04-25 15:05:01 +0200

    fix lacking -d flag in garbage collect automation

--- modules/global_packages.nix
+++ modules/global_packages.nix
@@ -8,8 +8,6 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    # ghostty # TODO: check if available on aarch64
-
     # nushell's in global_packages because it's
     # set as the root user's default shell
     nushell
