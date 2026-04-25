506beda878d1a85945d5a4d11b5e1727559caaa3
wint3rmute <mateusz.baczek1998@gmail.com>
2026-04-25 14:44:24 +0200

    helix config in separate toml file

--- modules/helix.nix
+++ modules/helix.nix
@@ -8,26 +8,7 @@
   programs.helix = {
     enable = true;
     defaultEditor = true;
-    # https://docs.helix-editor.com/configuration.html
-    extraConfig = ''
-      theme = "base16_transparent"
-      [editor]
-      line-number = "relative"
-
-      [keys.normal]
-      "0" = "goto_line_start"
-      "C-r" = "redo"
-      # "g" = ":reflow"
-      # ":e" = ":reload"
-
-      "}" = "goto_next_paragraph"
-      "{" = "goto_prev_paragraph"
-
-      [keys.normal.space]
-      "q" = ":quit"
-      "/" = "toggle_comments"
-      "s" = "global_search"
-    '';
+    extraConfig = builtins.readFile ./helix.toml;
     # Typescript, Deno, Helix
     # https://docs.deno.com/runtime/getting_started/setup_your_environment/#helix
     languages = {
