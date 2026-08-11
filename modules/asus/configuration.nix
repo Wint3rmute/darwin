a15aea1d2febffb6a7d2be7cbec32274b4804f52
Mateusz Bączek <mateusz.baczek1998@gmail.com>
2026-08-11 21:31:36 +0200

    testing llama cpp

--- modules/asus/configuration.nix
+++ modules/asus/configuration.nix
@@ -139,8 +139,12 @@
   services = {
     ollama = {
       enable = true;
-      package = pkgs.ollama-rocm;
+      # package = pkgs.ollama-rocm;
+      package = pkgs.ollama;
     };
+    # llama-cpp = {
+    #   enable = true;
+    # };
     # Enable the X11 windowing system.
     flatpak.enable = true;
     xserver = {
