d0d38b6c6f22b91218de9c809758797aba54ad60
Mateusz Bączek <mateusz.baczek1998@gmail.com>
2026-08-12 22:29:38 +0200

    trying to tweak llama-cpp

--- modules/asus/llm.nix
+++ modules/asus/llm.nix
@@ -7,6 +7,13 @@
     llama-cpp = {
       enable = true;
       package = pkgs.llama-cpp-rocm;
+      settings = {
+          # batch-size = 2048;
+          ctx-size = 16384;
+          flash-attn = "on";
+          host = "0.0.0.0";
+          # ubatch-size = 512;
+      };
     };
   };
 }
