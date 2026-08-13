2821e849c76a5761021a3e36b3ab15a19aa93da1
Mateusz Bączek <mateusz.baczek1998@gmail.com>
2026-08-13 20:45:25 +0200

    asus: muse still does not work

--- modules/asus/llm.nix
+++ modules/asus/llm.nix
@@ -8,11 +8,11 @@
       enable = true;
       package = pkgs.llama-cpp-rocm;
       settings = {
-          # batch-size = 2048;
-          ctx-size = 16384;
-          flash-attn = "on";
-          host = "0.0.0.0";
-          # ubatch-size = 512;
+        # batch-size = 2048;
+        ctx-size = 16384;
+        flash-attn = "on";
+        host = "0.0.0.0";
+        # ubatch-size = 512;
       };
     };
   };
