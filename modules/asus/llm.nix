e6fbe0f81585e57ead51c4d7e200d84bfbfeb04b
Mateusz Bączek <mateusz.baczek1998@gmail.com>
2026-08-15 13:40:34 +0200

    flake

--- modules/asus/llm.nix
+++ modules/asus/llm.nix
@@ -9,10 +9,18 @@
       package = pkgs.llama-cpp-rocm;
       settings = {
         # batch-size = 2048;
-        ctx-size = 16384;
+        # ubatch-size = 512;
+        ctx-size = 32768;
         flash-attn = "on";
         host = "0.0.0.0";
-        # ubatch-size = 512;
+        # "unsloth/Qwen3.8-27B-GGUF" = {
+        #   temp = 1.0;
+        #   top-k = 20;
+        #   top-p = 0.95;
+        #   min-p = 0.0;
+        #   presence-penalty = 0.0;
+        #   repetition-penalty=1.0;
+        # };
       };
     };
   };
